f6fbcd08381cf9fdfc24bd3043e4bb2e6a1b5900
Tristan Hart <tristanhart@gmail.com>
2016-11-29 07:01:22 +0000

    Add some page functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   
+  before_action :public_profile,    only: :show
+  
   def index
     # Display current user friends
   end
   
   def show
     # Display user wish list
+    
   end
+  
+  private
+  
+    def public_profile
+      @user = User.friendly.find(params[:id])
+      
+      # Redirect if profile isn't public
+      ##
+    end
 end
