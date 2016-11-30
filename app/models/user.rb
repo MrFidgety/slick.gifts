bf8b1c4c72a466f0775d5a5cf6a20f220271f6ad
Tristan Hart <tristanhart@gmail.com>
2016-11-30 07:10:56 +0000

    Change method at user signup/create

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,20 @@ class User < ActiveRecord::Base
                                   *(:developer if Rails.env.development?)]
                                   
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email if auth.info.email.present?
-      user.name = auth.info.name if auth.info.name.present?
-      user.image = auth.info.image if auth.info.image.present?
-      user.oauth_token = auth.credentials.token if auth.credentials.token.present?
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at) if auth.credentials.expires_at.present?
-    end
+    # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    #   user.email = auth.info.email if auth.info.email.present?
+    #   user.name = auth.info.name if auth.info.name.present?
+    #   user.image = auth.info.image if auth.info.image.present?
+    #   user.oauth_token = auth.credentials.token if auth.credentials.token.present?
+    #   user.oauth_expires_at = Time.at(auth.credentials.expires_at) if auth.credentials.expires_at.present?
+    # end
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.attributes = {
+      email: auth.info.email,
+      name: auth.info.name,
+      image: auth.info.image,
+      oauth_token: auth.credentials.token,
+      oauth_expires_at: Time.at(auth.credentials.expires_at)
+    }.save!
   end
   
   def is_friend?(user)
