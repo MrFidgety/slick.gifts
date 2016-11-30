19b235f1f54fd19550705f40160e25e21fd9ff5e
Tristan Hart <tristanhart@gmail.com>
2016-11-30 07:29:11 +0000

    Attempt omniauth update again

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   def sign_in_with(provider_name)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    @user.update_details(request.env["omniauth.auth"]).save
+    @user.read_from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user, :event => :authentication
     set_flash_message(:notice, :success, :kind => provider_name) if is_navigational_format?
     remember_me(@user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,22 +8,25 @@ class User < ActiveRecord::Base
                                   *(:developer if Rails.env.development?)]
                                   
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create
-    # do |user|
+    # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     #   user.email = auth.info.email if auth.info.email.present?
     #   user.name = auth.info.name if auth.info.name.present?
     #   user.image = auth.info.image if auth.info.image.present?
     #   user.oauth_token = auth.credentials.token if auth.credentials.token.present?
     #   user.oauth_expires_at = Time.at(auth.credentials.expires_at) if auth.credentials.expires_at.present?
     # end
+    
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize
+  
   end
   
-  def update_details(auth)
-    email = auth.info.email if auth.info.email.present?
-    name = auth.info.name if auth.info.name.present?
-    image = auth.info.image if auth.info.image.present?
-    oauth_token = auth.credentials.token if auth.credentials.token.present?
-    oauth_expires_at = Time.at(auth.credentials.expires_at) if auth.credentials.expires_at.present?
+  def read_from_omniauth(auth)
+    self.email = auth.info.email if auth.info.email.present?
+    self.name = auth.info.name if auth.info.name.present?
+    self.image = auth.info.image if auth.info.image.present?
+    self.oauth_token = auth.credentials.token if auth.credentials.token.present?
+    self.oauth_expires_at = Time.at(auth.credentials.expires_at) if auth.credentials.expires_at.present?
+    self.save!
   end
   
   def is_friend?(user)
