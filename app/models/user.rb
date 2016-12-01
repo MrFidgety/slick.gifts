fb21433995e6393448e81e46ae1e584e315ea143
Tristan Hart <tristanhart@gmail.com>
2016-12-01 00:37:13 +0000

    Update users before element modelling

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,13 @@ class User < ActiveRecord::Base
   devise  :rememberable, :trackable, :omniauthable, 
           :omniauth_providers => [:facebook, 
                                   *(:developer if Rails.env.development?)]
-                                  
-  def self.from_omniauth(auth)
-    # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    #   user.email = auth.info.email if auth.info.email.present?
-    #   user.name = auth.info.name if auth.info.name.present?
-    #   user.image = auth.info.image if auth.info.image.present?
-    #   user.oauth_token = auth.credentials.token if auth.credentials.token.present?
-    #   user.oauth_expires_at = Time.at(auth.credentials.expires_at) if auth.credentials.expires_at.present?
-    # end
-    
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize
   
+  # Find or create user from omniauth provider and uid                                
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize
   end
   
+  # Update user from omniauth hash
   def read_from_omniauth(auth)
     self.email = auth.info.email if auth.info.email.present?
     self.name = auth.info.name if auth.info.name.present?
@@ -29,6 +22,7 @@ class User < ActiveRecord::Base
     self.save!
   end
   
+  # Check if a specific user is a facebook friend
   def is_friend?(user)
     @graph = Koala::Facebook::API.new(oauth_token)
     @graph.get_connections("me", "friends/#{user.uid}").present?
