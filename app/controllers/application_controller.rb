8a9bedf50a80e6ef1363a5733cb9892248f2c558
Tristan Hart <tristanhart@gmail.com>
2016-12-06 01:23:21 +0000

    Test https url for SEO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
+  
+  # All urls are https by default
+  def default_url_options(options={})
+    { :secure => true }
+  end 
 
   def prepare_meta_tags(options={})
     site_name   = "Slick.gifts"
