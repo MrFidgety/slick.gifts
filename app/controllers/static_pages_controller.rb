c3cc8f9fd6c55ef86d52e7910028a7894c96a6ad
Tristan Hart <tristanhart@gmail.com>
2016-12-06 01:13:01 +0000

    SEO update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     # Set page meta tags
-    prepare_meta_tags(title: "Surprise! It's what you wanted. #NoBadGifts")
+    prepare_meta_tags(title: "Surprise! It's what you wanted. #NoBadGifts",
+                      description: "Make a list of the things you want, your interests, and style.  View your friends lists and never get a bad gift again.")
   end
 
   def help
