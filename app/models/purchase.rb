dae07f40f503437bf8ab74f47a9fead5d7fe8620
Tristan Hart <tristanhart@gmail.com>
2016-12-09 05:57:19 +0000

    Fix purchase error

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -10,6 +10,6 @@ class Purchase < ActiveRecord::Base
   end
   
   def set_status(new_status)
-    update_attribute(status, Purchase.statuses[new_status])
+    update_attribute(:status, Purchase.statuses[new_status])
   end
 end
