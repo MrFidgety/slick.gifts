8d1c4c9fd9e815a8c1600723ee747818fe6a110e
Tristan Hart <tristanhart@gmail.com>
2016-12-12 11:50:53 +0000

    User panel

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,12 +4,10 @@ module UsersHelper
   def user_bio(user)
     # Find an active interest
     want_interest = user.wants.where(wanted_type: "Interest", archived: false).order("RANDOM()").first
-    if want_interest
-      interest_name = want_interest.wanted.name
+    interest_name = want_interest.wanted.name if want_interest
     # Find an active item
     want_item = user.wants.where(wanted_type: "Item", archived: false).order("RANDOM()").first
-    if want_item
-      item_name = want_interest.wanted.name
+    item_name = want_interest.wanted.name if want_item
     
     bio = "Interested in #{interest_name} and wants #{item_name}"
   end
