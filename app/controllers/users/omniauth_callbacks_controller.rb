ccdecc8d202f635eb5cc4b8585f242858794b70a
Tristan Hart <tristanhart@gmail.com>
2016-12-17 05:39:03 +0000

    Fix redirect on signup error

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,10 +6,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def sign_in_with(provider_name)
     # Create or find user
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    # Set redirect to profile if new user
-    #store_location_for(:user, user_path(@user)) if @user.new_record?
+    # Check if new record
+    is_new_record = @user.new_record?
     # Read omniauth data into profile
     @user.read_from_omniauth(request.env["omniauth.auth"])
+    # Set redirect to profile if new record (after save)
+    store_location_for(:user, user_path(@user)) if is_new_record
     # Flash messages
     flash_message :facebook, "Successfully logged in with Facebook"
     flash_message :notice, "By the way, this is where we show you alerts"
